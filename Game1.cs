cd6311e3ef8501cee61f6a7ce805a7fe9d1697ab
DylanLale <disneygamer706@gmail.com>
2022-11-25 09:38:34 -0500

    need at add mouse click to switch between screens

--- Game1.cs
+++ Game1.cs
@@ -8,7 +8,22 @@ namespace Part_1_5__MonoGame_Assignment
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        enum Screen
+        {
+            Intro,
+            LeanStart,
+            LeanMiddle,
+            LeanLow,
+            LeanMiddleAgain,
+            StandingUp
+        }
+        Texture2D LeanStartTexture;
+        Texture2D LeanMiddleTexture;
+        Texture2D LeanLowTexture;
+        Texture2D LeanMiddleAgainTexture;
+        Texture2D StandingUp;
+        Screen screen;
+        MouseState mouseState;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,6 +34,9 @@ namespace Part_1_5__MonoGame_Assignment
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            screen = Screen.Intro;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
 
             base.Initialize();
         }
@@ -34,6 +52,13 @@ namespace Part_1_5__MonoGame_Assignment
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            mouseState = Mouse.GetState();
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.LeanStart;
+
+            }
 
             // TODO: Add your update logic here
 
@@ -42,7 +67,17 @@ namespace Part_1_5__MonoGame_Assignment
 
         protected override void Draw(GameTime gameTime)
         {
+            _spriteBatch.Begin();
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            if (screen == Screen.Intro)
+            {
+                
+            }
+
+            else if (screen == Screen.LeanStart)
+            {
+               
+            }
 
             // TODO: Add your drawing code here
 
